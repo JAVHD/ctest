b4abcaefd2bd6443c49a9a22e78c95ffb89654a0
bear <593788753@qq.com>
2019-03-30 15:38:00 +0800

    enum

---
+++ define.c
@@ -0,0 +1,22 @@
+/*
+* @Author: bear
+* @Date:   2019-03-26 01:16:31
+* @Last Modified by:   bear
+* @Last Modified time: 2019-03-26 01:23:56
+*/
+
+
+#include <stdio.h>
+
+#define STRING char *
+
+
+int main()
+{
+
+	STRING name, brand;
+
+	//printf("%c\n", );
+
+	return 0;
+}
